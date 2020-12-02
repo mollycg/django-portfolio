613172050c7942b23340ce74bc5c722d674b6c1b
Molly Geerling <mollygeerling@gmail.com>
2020-12-02 13:06:57 -0600

    update styling and formatting of elements

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    image = models.ImageField(upload_to='portfolio/images/')
+    date = models.DateField(blank=True)
+    image = models.ImageField(upload_to='portfolio/images/', blank=True)
     url = models.URLField(blank=True)
+    repo = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
